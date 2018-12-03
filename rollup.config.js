7999a59b27582fe5bfc0044f12a27034b01ce660
HenriqueLimas <henrique.ramos.limas@gmail.com>
2018-12-03 19:29:52 +0100

    feat(lifecycles): Add on attributes changed handler

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,42 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 
-export default [
+const modules = [
   {
-    input: "./lib/index.js",
-    output: [
-      {
-        name: "osagai",
-        file: "./dist/osagai.js",
-        format: "cjs"
-      },
-      {
-        name: "osagai",
-        file: "./dist/osagai.mjs",
-        format: "esm"
-      },
-      {
-        name: "osagai",
-        file: "./dist/osagai.umd.js",
-        format: "umd"
-      }
-    ],
-    plugins: [resolve({ browser: true }), commonjs()]
+    name: "osagai",
+    fileName: "osagai",
+    input: "./lib/index.js"
   },
   {
-    input: "./lib/events/index.js",
-    output: [
-      {
-        name: "osagai/events",
-        file: "./dist/events.js",
-        format: "cjs"
-      },
-      {
-        name: "osagai/events",
-        file: "./dist/events.mjs",
-        format: "esm"
-      },
-      {
-        name: "osagai/events",
-        file: "./dist/events.umd.js",
-        format: "umd"
-      }
-    ],
-    plugins: [resolve({ browser: true }), commonjs()]
+    name: "osagai/events",
+    fileName: "events",
+    input: "./lib/events/index.js"
+  },
+  {
+    name: "osagai/lifecycles",
+    fileName: "lifecycles",
+    input: "./lib/lifecycles/index.js"
   }
 ];
+
+export default modules.map(moduleConfig => ({
+  input: moduleConfig.input,
+  output: [
+    {
+      name: moduleConfig.name,
+      file: `./dist/${moduleConfig.fileName}.js`,
+      format: "cjs"
+    },
+    {
+      name: moduleConfig.name,
+      file: `./dist/${moduleConfig.fileName}.mjs`,
+      format: "esm"
+    },
+    {
+      name: moduleConfig.name,
+      file: `./dist/${moduleConfig.fileName}.umd.js`,
+      format: "umd"
+    }
+  ],
+  plugins: [resolve({ browser: true }), commonjs()]
+}));
