af49338f565d54ac2179c3db746084b68651a44f
HenriqueLimas <henrique.ramos.limas@gmail.com>
2018-12-22 10:37:18 +0100

    dom(update): start using lit-html

--- demo/todomvc/src/td-model.js
+++ demo/todomvc/src/td-model.js
@@ -1,59 +1,6 @@
 function TdModel({ element }) {
   const STORAGE_KEY = "model";
 
-  Object.assign(element, {
-    todos: JSON.parse(localStorage.getItem(STORAGE_KEY)) || [],
-    addTodo: title => {
-      element.todos.push({
-        id: Date.now().toString(),
-        title,
-        completed: false
-      });
-
-      updateStorage(element);
-
-      return element.todos;
-    },
-    removeTodo: id => {
-      element.todos = element.todos.filter(todo => todo.id !== id);
-      updateStorage(element);
-      return element.todos;
-    },
-    toggleTodo: id => {
-      element.todos = element.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-
-        return todo;
-      });
-
-      updateStorage(element);
-
-      return element.todos;
-    },
-    filter: (filter = "all") => {
-      switch (filter) {
-        case "all":
-          return element.todos;
-        case "active":
-          return element.filterActive();
-        case "completed":
-          return element.filterCompleted();
-        default:
-          return element.todos;
-      }
-    },
-    filterCompleted: () => element.todos.filter(todo => todo.completed),
-    filterActive: () => element.todos.filter(todo => !todo.completed),
-    removeCompleted: () => {
-      element.todos = element.filterActive();
-      updateStorage(element);
-      return element.todos;
-    },
-    hasCompleted: () => element.filterCompleted().length
-  });
-
   const updateStorage = element => {
     Promise.resolve().then(() => {
       localStorage.setItem(STORAGE_KEY, JSON.stringify(element.todos));
@@ -61,7 +8,60 @@ function TdModel({ element }) {
     });
   };
 
-  return () => "";
+  return () => {
+    Object.assign(element, {
+      todos: JSON.parse(localStorage.getItem(STORAGE_KEY)) || [],
+      addTodo: title => {
+        element.todos.push({
+          id: Date.now().toString(),
+          title,
+          completed: false
+        });
+
+        updateStorage(element);
+
+        return element.todos;
+      },
+      removeTodo: id => {
+        element.todos = element.todos.filter(todo => todo.id !== id);
+        updateStorage(element);
+        return element.todos;
+      },
+      toggleTodo: id => {
+        element.todos = element.todos.map(todo => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed;
+          }
+
+          return todo;
+        });
+
+        updateStorage(element);
+
+        return element.todos;
+      },
+      filter: (filter = "all") => {
+        switch (filter) {
+          case "all":
+            return element.todos;
+          case "active":
+            return element.filterActive();
+          case "completed":
+            return element.filterCompleted();
+          default:
+            return element.todos;
+        }
+      },
+      filterCompleted: () => element.todos.filter(todo => todo.completed),
+      filterActive: () => element.todos.filter(todo => !todo.completed),
+      removeCompleted: () => {
+        element.todos = element.filterActive();
+        updateStorage(element);
+        return element.todos;
+      },
+      hasCompleted: () => element.filterCompleted().length
+    });
+  };
 }
 
 export default osagai.define("td-model", TdModel);
--- demo/todomvc/src/td-todos.js
+++ demo/todomvc/src/td-todos.js
@@ -1,77 +1,61 @@
-import { on } from "../node_modules/osagai/dist/events.mjs";
+import { html, update } from "../node_modules/osagai/dom.mjs";
 
-function TdTodos({ element, query, update }) {
-  update(async (currentState = {}) => {
+function TdTodos({ element, query }) {
+  update(element, async (currentState = {}) => {
     const model = await query("td-model");
     currentState.getTodos = model.filter;
     currentState.hasCompleted = model.hasCompleted;
     return currentState;
   });
 
-  on("submit", query(".js-form"), async event => {
+  const handleSubmit = async event => {
     event.preventDefault();
     const value = event.target.elements["new-todo"].value;
 
-    update(async currentState => {
+    update(element, async currentState => {
       const model = await query("td-model");
       model.addTodo(value);
       return currentState;
     });
-  });
+  };
 
-  on("click", element, async event => {
+  const handleRemove = event => {
     const elm = event.target;
+    const id = elm.dataset.id;
 
-    if (elm.classList.contains("destroy")) {
-      const id = elm.dataset.id;
-      return update(async currentState => {
-        const model = await query("td-model");
-        model.removeTodo(id);
-        return currentState;
-      });
-    }
+    update(element, async currentState => {
+      const model = await query("td-model");
+      model.removeTodo(id);
+      return currentState;
+    });
+  };
 
-    if (elm.classList.contains("toggle")) {
-      const id = elm.dataset.id;
-      return update(async currentState => {
-        const model = await query("td-model");
-        model.toggleTodo(id);
-        return currentState;
-      });
-    }
+  const handleToggle = event => {
+    const elm = event.target;
+    const id = elm.dataset.id;
 
-    if (elm.classList.contains("js-all")) {
-      event.preventDefault();
-      return update(currentState => {
-        currentState.filter = "all";
-        return currentState;
-      });
-    }
+    update(element, async currentState => {
+      const model = await query("td-model");
+      model.toggleTodo(id);
+      return currentState;
+    });
+  };
 
-    if (elm.classList.contains("js-active")) {
-      event.preventDefault();
-      return update(currentState => {
-        currentState.filter = "active";
-        return currentState;
-      });
-    }
+  const handleFilter = filter => event => {
+    event.preventDefault();
+    update(element, currentState => {
+      currentState.filter = filter;
+      return currentState;
+    });
+  };
 
-    if (elm.classList.contains("js-completed")) {
-      event.preventDefault();
-      return update(currentState => {
-        currentState.filter = "completed";
-        return currentState;
-      });
-    }
-
-    if (elm.classList.contains("clear-completed")) {
-      return update(async currentState => {
-        const model = await query("td-model");
-        model.removeCompleted();
-        return currentState;
-      });
-    }
-  });
+  const handleClearCompleted = () => {
+    update(element, async currentState => {
+      const model = await query("td-model");
+      model.removeCompleted();
+      return currentState;
+    });
+  };
 
   return ({
     getTodos = () => [],
@@ -79,76 +63,107 @@ function TdTodos({ element, query, update }) {
     hasCompleted = () => false,
     todos = getTodos(filter)
   } = {}) =>
-    `<section>
-      <td-model></td-model>
+    html`
+      <section>
+        <td-model></td-model>
 
-      <header class="header">
-        <form class="js-form">
-          <input id="new-todo" name="new-todo" class="new-todo" placeholder="What needs to be done" autofocus autocomplete="off">
-        </form>
-      </header>
+        <header class="header">
+          <form @submit=${handleSubmit}>
+            <input
+              id="new-todo"
+              name="new-todo"
+              class="new-todo"
+              placeholder="What needs to be done"
+              autofocus
+              autocomplete="off"
+            />
+          </form>
+        </header>
 
-      <main class="main">
-        <ul class="todo-list">
-          ${todos
-            .map(
-              item =>
-                `<li data-id="${item.id}" class="${
-                  item.completed ? "completed" : ""
-                }">
-                  <div class="view">
-                    <input data-id="${
-                      item.id
-                    }" class="toggle" type="checkbox" ${
-                  item.completed ? "checked" : ""
-                }>
-                    <label>${item.title}</label>
-                    <button data-id="${item.id}" class="destroy"></button>
-                  </div>
-                </li>`
-            )
-            .join("")}
-        </ul>
-      </main>
-
-      ${
-        todos.length || filter === "completed"
-          ? `
-      <footer class="footer">
-        <span class="todo-count">
-          <strong>${todos.length}</strong>
-          <span>item${todos.length > 1 ? "s" : ""}</span>
-          <span>left</span>
-        </span>
-
-        <ul class="filters">
-          <li>
-            <a href="#/" class="js-all ${
-              filter === "all" ? "selected" : ""
-            }">All</a>
-          </li>
-          <li>
-            <a href="#/active" class="js-active ${
-              filter === "active" ? "selected" : ""
-            }">Active</a>
-          </li>
-          <li>
-            <a href="#/completed" class="js-completed ${
-              filter === "completed" ? "selected" : ""
-            }">Completed</a>
-          </li>
-        </ul>
+        <main class="main">
+          <ul class="todo-list">
+            ${
+              todos.map(
+                item =>
+                  html`
+                    <li
+                      data-id="${item.id}"
+                      class="${item.completed ? "completed" : ""}"
+                    >
+                      <div class="view">
+                        <input
+                          data-id="${item.id}"
+                          class="toggle"
+                          type="checkbox"
+                          @click=${handleToggle}
+                          checked="${item.completed}"
+                        />
+                        <label>${item.title}</label>
+                        <button
+                          data-id="${item.id}"
+                          @click=${handleRemove}
+                          class="destroy"
+                        ></button>
+                      </div>
+                    </li>
+                  `
+              )
+            }
+          </ul>
+        </main>
 
         ${
-          hasCompleted()
-            ? `<button class="clear-completed">Clear completed</button>`
+          todos.length || filter === "completed"
+            ? html`
+                <footer class="footer">
+                  <span class="todo-count">
+                    <strong>${todos.length}</strong>
+                    <span>item${todos.length > 1 ? "s" : ""}</span>
+                    <span>left</span>
+                  </span>
+
+                  <ul class="filters">
+                    <li>
+                      <a
+                        href="#/"
+                        class="${filter === "all" ? "selected" : ""}"
+                        @click=${handleFilter("all")}
+                        >All</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        href="#/active"
+                        class="${filter === "active" ? "selected" : ""}"
+                        @click=${handleFilter("active")}
+                        >Active</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        href="#/completed"
+                        class="${filter === "completed" ? "selected" : ""}"
+                        @click=${handleFilter("completed")}
+                        >Completed</a
+                      >
+                    </li>
+                  </ul>
+
+                  ${
+                    hasCompleted()
+                      ? html`
+                          <button class="clear-completed">
+                            Clear completed
+                          </button>
+                        `
+                      : ""
+                  }
+                </footer>
+              `
             : ""
         }
-      </footer>
-      `
-          : ""
-      }
-    </section>`;
+      </section>
+    `;
 }
 
 export default osagai.define("td-todos", TdTodos);
