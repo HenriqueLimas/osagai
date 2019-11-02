028f8be8d29dd29aed7ca26d06d7da79d3baccee
HenriqueLimas <henrique.ramos.limas@gmail.com>
2019-11-03 00:08:57 +0100

    feat(core): add shadow dom support on queries functions

--- test/dom/attachShadow.test.js
+++ test/dom/attachShadow.test.js
@@ -38,4 +38,48 @@ describe("attachShadow()", () => {
       "Ford Prefect"
     );
   });
+
+  it("should be able to query the child element", async () => {
+    let _query;
+
+    const Component = ({ element, query }) => {
+      attachShadow(element);
+      _query = query;
+
+      return () => `<div class="name">Joe</div>`;
+    };
+
+    define("shadow-root-query", Component);
+
+    const element = document.createElement("shadow-root-query");
+    document.body.appendChild(element);
+
+    const name = await _query(".name");
+
+    expect(name.textContent).toBe("Joe");
+  });
+
+  it("should be able to query all the children elements", async () => {
+    let _queryAll;
+
+    const Component = ({ element, queryAll }) => {
+      attachShadow(element);
+      _queryAll = queryAll;
+
+      return () => `<ul>
+        <li class="item">Item 1</li>
+        <li class="item">Item 2</li>
+      </ul>`;
+    };
+
+    define("shadow-root-query-all", Component);
+
+    const element = document.createElement("shadow-root-query-all");
+    document.body.appendChild(element);
+
+    const items = await _queryAll(".item");
+
+    expect(items[0].textContent).toBe("Item 1");
+    expect(items[1].textContent).toBe("Item 2");
+  });
 });
